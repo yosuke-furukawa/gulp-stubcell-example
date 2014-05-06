f04839870ec9779f40f319ed1ec0d69fd5ab3b61
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-05-07 02:41:41 +0900

    Add example

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var stubcell = require('gulp-stubcell');
+var connect = require('gulp-connect');
+var proxy = require('proxy-middleware');
+var url = require('url');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: ['build'],
+    port: 9000,
+    livereload: true,
+    middleware: function(connect, o) {
+        return [ (function() {
+            var options = url.parse('http://localhost:3000/test');
+            options.route = '/test';
+            return proxy(options);
+        })() ];
+    }
+  });
+});
+
+gulp.task('stubcell', function() {
+  stubcell.start();
+});
+
+gulp.task('default', ['connect', 'stubcell']);
